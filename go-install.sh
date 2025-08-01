5f0854d7812204d4a0e06f54b7323d868a8844ce
Leandro Ascaino <leandro.ascaino@gmail.com>
2025-08-01 15:11:20 -0300

    Changes on Install script

--- go-install.sh
+++ go-install.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+
+if [[ "$(basename "$PWD")" != "go" ]];
+then
+    printf "\e[0m\n"
+    printf "\e[31m You need to execute under “go” folder \n"
+    printf "\e[0m\n"
+    exit 1;
+fi
+
 # Instructions for installing go through GVM tool within a warp container
-warp php ssh --root
 apt update
 apt-get install bsdmainutils
 apt-get install bison
@@ -11,10 +19,25 @@ gvm install go1.24.5 -B
 gvm use go1.24.5 --default
 go mod tidy
 
+# creating log file
+mkdir var/
+mkdir var/log/
+touch var/log/app.log
+chown warp:www-data var/ -R
+printf "\e[0m\n"
+printf "\e[36m Log file created on: \n"
+printf "\e[0m var/log/app.log \n"
+
 # Refresh file to use "go" commands
-echo "Please restart your terminal session or to get started right away run
-       `source /root/.gvm/scripts/gvm`"
+printf "\e[0m\n"
+printf "\e[36m Please restart your terminal session or to get started right away run: \n"
+printf "\e[0m source /root/.gvm/scripts/gvm \n"
 
 # Run application
-go run magento.go
+printf "\e[0m\n"
+printf "\e[36m Start GoGento application: \n"
+printf "\e[0m go run magento.go \n"
+printf "\e[0m\n"
+
+
 
